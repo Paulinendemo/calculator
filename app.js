b3d866c16dd9309dcbf41612bd3563acf730f584
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-07-20 09:35:53 +0300

    final commit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-let x=parseInt(document)
+let output=document.getElementById("output");
+let buttons=Array.from( document.getElementsByClassName("button"))
+buttons.map(button=>{
+    button.addEventListener('click',(e)=>{
+        switch(e.target.innerText){
+            case 'C':
+                output.innerText='';
+                break;
+            case '←':
+                if (output.innerText){
+                    output.innerText=output.innerText.slice(0,-1);
+                } 
+                break;   
+            case '=':
+                output.innerText =eval(output.innerText)
+                break; 
+            default:
+                output.innerText+=e.target.innerText;    
+            
+        }
+    })
+})
+    
